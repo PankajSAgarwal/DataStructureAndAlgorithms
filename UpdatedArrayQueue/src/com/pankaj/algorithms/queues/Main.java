154d56057489c109cea4051cf53a6186cbf2b51d
Agarwal, Pankaj <2005pank@gmail.com>
2019-12-26 09:22:46 +0530

    Circular queues

---
+++ UpdatedArrayQueue/src/com/pankaj/algorithms/queues/Main.java
@@ -0,0 +1,46 @@
+package com.pankaj.algorithms.queues;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Employee janeJones = new Employee("Jane","Jones",123);
+        Employee johnDoe = new Employee("John","Doe",4567);
+        Employee marySmith = new Employee("Mary","Smith",22);
+        Employee mikeWilson = new Employee("Mike","Wilson",3245);
+        Employee billEnd = new Employee("Bill","End",78);
+
+        ArrayQueue queue = new ArrayQueue(5);
+
+        // Circular queue - Wrap queue condition
+//        queue.add(janeJones);
+//        queue.add(johnDoe);
+//        queue.remove();
+//        queue.add(marySmith);
+//        queue.remove();
+//        queue.add(mikeWilson);
+//        queue.remove();
+//        queue.add(billEnd);
+//        queue.remove();
+//        queue.add(janeJones);
+
+        // No wrapping of queue required here
+        queue.add(janeJones);
+        queue.add(johnDoe);
+        queue.add(marySmith);
+        queue.add(mikeWilson);
+        queue.add(billEnd);
+
+        queue.remove();
+        queue.remove();
+        //System.out.println(queue.peek());
+        queue.remove();
+        queue.remove();
+        queue.remove();
+        //queue.remove();
+
+        queue.add(mikeWilson);
+        queue.printQueue();
+
+
+    }
+}
